6b1f552b18f03e7085f06e9aae879b79c2d15457
libertalia1724 <libertalia1724@gmail.com>
2026-05-28 10:59:00 +0300

    generate proto

--- x/auth/vesting/types/tx.pb.go
+++ x/auth/vesting/types/tx.pb.go
@@ -804,6 +804,7 @@ func _Msg_DonateAllVestingTokens_Handler(srv interface{}, ctx context.Context, d
 	return interceptor(ctx, in, info, handler)
 }
 
+var Msg_serviceDesc = _Msg_serviceDesc
 var _Msg_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "cosmos.vesting.v1beta1.Msg",
 	HandlerType: (*MsgServer)(nil),
